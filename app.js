54a12b36d0527a9db25647422b68a118ad29ddff
Jerry <vlflalektzja@naver.com>
2019-09-01 23:23:08 +0900

    Begin

---
+++ app.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan('[:date[iso]] :method :status :url :response-time(ms) :user-agent'));
+app.use(express.static(path.join(__dirname,'static')));
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/api',require('./api'));
+app.use('/',require('./router'));
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000!');
+});
